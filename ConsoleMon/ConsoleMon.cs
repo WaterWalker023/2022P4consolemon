8928cb5e89d4805c901b2aedd3ad77f4bdaba7ec
WaterWalker023 <32755@ma-web.nl>
2022-05-09 10:33:15 +0200

    consolemon

---
+++ ConsoleMon/ConsoleMon.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleMon
+{
+    internal class ConsoleMon
+    {
+        int hp;
+        int energy;
+        string name;
+
+    }
+}
